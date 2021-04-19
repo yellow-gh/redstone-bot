865dd7ae6890cf669a0c9d4457edd2c6116b9d6f
yellow <75477334+yellow-gh@users.noreply.github.com>
2021-04-19 19:06:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,7 @@
 import discord
 from discord.ext import commands, tasks
-from discord import Guild
-from discord.voice_client import VoiceClient
-import youtube_dl
 import asyncio
 import random
-import time
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
 
 antworten = ['Ja', 'Nein', 'Vielleicht', 'Wahrscheinlich', 'Sieht so aus', 'Sehr wahrscheinlich', 'Sehr unwahrscheinlich']
 teilnehmer = []
@@ -65,67 +17,14 @@ async def on_ready():
     print('Bot wurde gestartet')
 
 @bot.event
-async def on_member_join(member):
-    if not member.bot:
-        guild = bot.get_guild(597787927476699157)
-        role = guild.get_role(722031383362404382)
-        role2 = guild.get_role(789520194552332308)
-        await member.add_roles(role)
-        await member.add_roles(role2)
-        embed = discord.Embed(title="Willkommen im Redstone Tab {} ".format(member.name),description='von redstone bot', color=random.choice(embedcollor))
-        embed.add_field(name="Hallo auch von meiner seite, ich Organisiere den ganzen Kram am server. Aber nun viel spaß im Redstone Tab",value='** **',inline=True)
-        try:
-            if not member.dm_channel:
-                await member.create_dm()
-            await member.dm_channel.send(embed=embed)
-        except discord.errors.Forbidden:
-            print('Es konnte keine Willkommensnachricht an {} gesendet werden.'.format(member.name))
-
-@bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
         embed.add_field(name='Befehl nicht gefunden',value='** **',inline=True)
         await ctx.send(embed=embed)
 
-
 @bot.event
 async def on_raw_reaction_add(payload):
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "📢":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(789542743726620712)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🎉":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(789530915268853811)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "❓":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(790639841184055316)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🟥":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(792113253421154314)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🤖":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(806094937887342603)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-
     if payload.channel_id == 789478285205569559:
         guild = bot.get_guild(597787927476699157)
         mem = discord.utils.find(lambda m : m.id == 708230773219786793, guild.members)
@@ -134,51 +33,11 @@ async def on_raw_reaction_add(payload):
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
             teilnehmer.append(member)
 
-@bot.event
-async def on_raw_reaction_remove(payload):
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "📢":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(789542743726620712)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🎉":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(789530915268853811)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "❓":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(790639841184055316)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🟥":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(792113253421154314)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-
-    if payload.channel_id == 789531336187707422:
-        if payload.emoji.name == "🤖":
-            guild = bot.get_guild(597787927476699157)
-            role = guild.get_role(806094937887342603)
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-
 @tasks.loop(seconds=10)
 async def change_status():
     await bot.change_presence(activity=discord.Game('ich spiele nicht ich arbeite'), status=discord.Status.online)
     await asyncio.sleep(5)
     await bot.change_presence(activity=discord.Game('[r!help]'), status=discord.Status.online)
-    guild = bot.get_guild(597787927476699157)
-    channel = guild.get_channel(791016242268864552)
-    await channel.edit(name='«👥»Membercount : {}'.format(guild.member_count))
 
 @bot.command()
 async def help(ctx):
@@ -187,14 +46,8 @@ async def help(ctx):
         embed.set_footer(text=f'angefordert von {ctx.author}')
         embed.add_field(name='**gstart** [text]',value='Startet ein Gewinnspiel (nur für Berechtigte)',inline=True)
         embed.add_field(name='**gchose**',value='wertet einen Gewinner aus (nur für Berechtigte)',inline=True)
-        embed.add_field(name='**play** [lied]',value='Spielt Musik ab (nur für Berechtigte)',inline=True)
-        embed.add_field(name='**stop**',value='Stoppt das abspielen von Musik (nur für Berechtigte)',inline=True)
-        embed.add_field(name='**pause**',value='Pausiert das abspielen von Musik (nur für Berechtigte)',inline=True)
-        embed.add_field(name='**resume**',value='Setzt das abspielen von Musik fort (nur für Berechtigte)',inline=True)
         embed.add_field(name='**userinfo** [user]',value='Zeigt Informationen über User an',inline=True)
-        embed.add_field(name='**clear** [anzahl]',value='Löscht Nachrichten (nur für Berechtigte)',inline=True)
         embed.add_field(name='**8ball** [frage]',value='Wahrsagefunktion',inline=True)
-        embed.add_field(name='**temp**',value='Zeigt die Temperatur des servers vom bot an',inline=True)
         embed.add_field(name='**ping**',value='zur überprüfung der Latenz',inline=True)
         embed.add_field(name='**embed** [text]',value='sendet ein embed (nur für Berechtigte)',inline=True)
         embed.add_field(name='**flip**',value='coinflip funktion',inline=True)
@@ -230,7 +83,6 @@ async def embed(ctx, *, text):
     embed.set_thumbnail(url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
 
-792113253421154314
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def gstart(ctx, *, arg):
@@ -282,106 +134,6 @@ async def gchose_error(ctx, error):
         await ctx.send(embed=embed)
 
 @bot.command()
-@commands.has_role(784843450025508905)
-async def play(ctx, *, url):
-    guild = bot.get_guild(597787927476699157)
-    member = discord.utils.find(lambda m : m.id == 708230773219786793, guild.members)
-    if not member.voice:
-        if not ctx.message.author.voice:
-            embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-            embed.add_field(name='Du bist mit keinem Sprachkanal verbunden',value='** **',inline=True)
-            await ctx.send(embed=embed)
-            return
-
-        else:
-            channel = ctx.message.author.voice.channel
-
-        await channel.connect()
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    if not voice_channel.is_playing():
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-        embed = discord.Embed(title='**Spielt jetzt:** ', description='**{}**'.format(player.title),color=random.choice(embedcollor))
-        embed.set_footer(text='hinzugefügt von {}'.format(ctx.author))
-        await ctx.send(embed=embed)
-
-    else:
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='du kannst aktuel keine Musik abspielen', value='** **', inline=True)
-        await ctx.send(embed=embed)
-
-@play.error
-async def play_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du darft diesen Befehl nicht benutzen', value='** **', inline=True)
-        await ctx.send(embed=embed)
-    elif isinstance(error, commands.MissingRequiredArgument):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du hast kein Musikstück angegeben', value='** **', inline=True)
-        await ctx.send(embed=embed)
-
-@bot.command()
-@commands.has_role(784843450025508905)
-async def pause(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    embed = discord.Embed(title='**Player pausiert** ',color=random.choice(embedcollor))
-    await ctx.send(embed=embed)
-
-    voice_channel.pause()
-
-@pause.error
-async def pause_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du darft diesen Befehl nicht benutzen',value='** **',inline=True)
-        await ctx.send(embed=embed)
-
-@bot.command()
-@commands.has_role(784843450025508905)
-async def resume(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    embed = discord.Embed(title='**Player fortgesetzt** ',color=random.choice(embedcollor))
-    await ctx.send(embed=embed)
-
-    voice_channel.resume()
-
-@resume.error
-async def resume_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du darft diesen Befehl nicht benutzen',value='** **',inline=True)
-        await ctx.send(embed=embed)
-
-@bot.command(name='stop')
-@commands.has_role(784843450025508905)
-async def stop_(ctx):
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-    voice_client = ctx.message.guild.voice_client
-    embed = discord.Embed(title='**Player gestopt** ',color=random.choice(embedcollor))
-    await ctx.send(embed=embed)
-
-    voice_channel.stop()
-    await voice_client.disconnect()
-
-@stop_.error
-async def stop_error(ctx, error):
-    if isinstance(error, commands.MissingRole):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du darft diesen Befehl nicht benutzen',value='** **',inline=True)
-        await ctx.send(embed=embed)
-
-@bot.command()
 async def userinfo(ctx, member : discord.Member):
     try:
         embed = discord.Embed(title='Userinfo für {}'.format(member.name),description='Dies ist eine Userinfo für den User {}'.format(member.mention),color=random.choice(embedcollor))
@@ -403,22 +155,6 @@ async def userinfo_error(ctx, error):
     elif isinstance(error, commands.BadArgument):
         embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
         embed.add_field(name='User wurde nicht gefunden',value='** **',inline=True)
-        await ctx.send(embed=embed)
-
-@bot.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=1):
-    count = amount + 1
-    await ctx.channel.purge(limit=count)
-    embed = discord.Embed(title='**Clear**', description='{0} Nachrichten wurden gelöscht'.format(amount),color=random.choice(embedcollor))
-    embed.set_footer(text=f'angefordert von {ctx.author}')
-    await ctx.send(embed=embed, delete_after=5.0)
-
-@clear.error
-async def clear_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        embed = discord.Embed(title='**Error** ',color=random.choice(embedcollor))
-        embed.add_field(name='Du darft diesen Befehl nicht benutzen',value='** **',inline=True)
         await ctx.send(embed=embed)
 
 @bot.command(name='8ball')
@@ -443,16 +179,6 @@ async def ball_error(ctx, error):
         await ctx.send(embed=embed)
 
 @bot.command()
-async def temp(ctx):
-    tempData = "/sys/class/thermal/thermal_zone0/temp"
-    dateilesen = open(tempData, "r")
-    temperatur = dateilesen.readline(2)
-    dateilesen.close()
-    embed = discord.Embed(title='Temperatur', description='Die CPU hat {} Grad'.format(temperatur),color=random.choice(embedcollor))
-    embed.set_footer(text=f'angefordert von {ctx.author}')
-    await ctx.send(embed=embed)
-
-@bot.command()
 async def ping(ctx):
     embed = discord.Embed(title='**Pong!**', description=f'Latenz: {round(bot.latency * 1000)}ms',color=random.choice(embedcollor))
     embed.set_footer(text=f'angefordert von {ctx.author}')
@@ -463,5 +189,3 @@ async def flip(ctx):
     embed = discord.Embed(title='**Coinflip**', description='{}'.format(random.choice(coinflip)),color=random.choice(embedcollor))
     embed.set_footer(text=f'angefordert von {ctx.author}')
     await ctx.send(embed=embed)
-
-bot.run('')
